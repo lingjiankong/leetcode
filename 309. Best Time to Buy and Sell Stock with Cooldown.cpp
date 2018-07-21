47ebd2f585b45af071b53c3aff8f326a9813755c
andykong10 <andykong10@github.com>
2018-07-20 17:37:13 -0700

    update

---
+++ 309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,35 @@
+// The DP problem can be modeled as a MDP, You will understand everything once you see the MDP diagram is Huahua's video: http://zxi.mytechroad.com/blog/dynamic-programming/leetcode-309-best-time-to-buy-and-sell-stock-with-cooldown/
+
+// at any day, we keep three states (shown below):
+// three actions that we can perform to transit between these states are: buy, sell, do nothing
+
+// hold[i]: the amount of money you have if on day i you hold the stock you've bought. hold = max(prev_hold, prev_rest-price): you can transit to hold from previous hold via doing nothing, or you can transit to hold from previous rest via buying a stock (that's why we have -price)
+
+// sold[i]: the amount of money you have if on day i you sell the stock that you've bought, sold = prev_hold + price: you can transit to sold only via selling the stock you have (that's why we have +price)
+
+// rest[i]: the amount of money you have if on day i you rest (with no stock on hand), rest = max(prev_rest, prev_sold): you can transit to rest either from previous rest by doing nothing, or from previous sold by doing nothing (i.e. cooldown)
+
+// in the end, the maximum profit only occurs either in state sold or rest. If you hold any stock on hand, that stock has not been cashed!
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int hold = INT_MIN;
+        int sold = 0;
+        int rest = 0;
+
+        for (auto price : prices) {
+
+            int prev_hold = hold;
+            int prev_sold = sold;
+            int prev_rest = rest;
+
+            hold = max(prev_hold, prev_rest-price);
+            sold = prev_hold + price;
+            rest = max(prev_rest, prev_sold);
+        }
+
+        return max(sold, rest);
+    }
+};
