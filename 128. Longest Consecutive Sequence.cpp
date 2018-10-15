9eb3ab1f527e48d03ae0f75ee01974aff52cade2
Andy Kong <andy.k@aiincorporated.com>
2018-10-14 23:29:23 -0700

    Update.

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -1,26 +1,36 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
+class Solution
+{
 
-        unordered_set<int> seen(nums.begin(), nums.end());
-        int res = 0;
+	public:
 
-        for (int num : nums) {
-            int up = num + 1;
-            while (seen.find(up) != seen.end()) {
-                seen.erase(up);
-                ++up;
-            }
+		int longestConsecutive(vector<int>& nums)
+		{
 
-            int down = num - 1;
-            while (seen.find(down) != seen.end()) {
-                seen.erase(down);
-                --down;
-            }
+			// Use unordered_set to eliminate duplicate elements
+			unordered_set<int> seen(nums.begin(), nums.end());
+			int res = 0;
 
-            res = max(res, up-down-1);
-        }
+			for (int num : nums)
+			{
+				// Find those that are greater than num
+				int up = num + 1;
+				while (seen.find(up) != seen.end())
+				{
+					seen.erase(up);
+					++up;
+				}
 
-        return res;
-    }
+				// Find those that are less than num
+				int down = num - 1;
+				while (seen.find(down) != seen.end())
+				{
+					seen.erase(down);
+					--down;
+				}
+
+				res = max(res, up-down-1);
+			}
+
+			return res;
+		}
 };
--- 334. Increasing Triplet Subsequence.cpp
+++ 334. Increasing Triplet Subsequence.cpp
@@ -1,21 +1,37 @@
-class Solution {
-public:
-    bool increasingTriplet(vector<int>& nums) {
+class Solution
+{
 
-        int min = INT_MAX; // most minimum element
-        int secondMin = INT_MAX; // second most minimum element
+	public:
 
-        // <= (not <) is important, because we want increasing orders. If we want non-decreasing orders, we can do <
-        for (int num : nums) {
-            if (num <= min) {
-                min = num;
-            } else if (num <= secondMin) {
-                secondMin = num;
-            } else {
-                return true;
-            }
-        }
+		bool increasingTriplet(vector<int>& nums) {
 
-        return false;
-    }
+			int min = INT_MAX; // Most minimum element
+			int secondMin = INT_MAX; // Second most minimum element
+
+			// Note: <= (not <) is important, because we want increasing orders. 
+			// If we want non-decreasing orders, we can do <
+			for (int num : nums)
+			{
+				if (num <= min)
+				{
+					// num is the most minimum element seen so far
+					min = num;
+				}
+				else if (num <= secondMin)
+				{
+					// num is the second most minimum element seen so far
+					// If we enter this else if loop, then we are guaranteed that there exists 
+					// the most minimum element (min) that occurs before num (now secondMin)
+					secondMin = num;
+				}
+				else
+				{
+					// Same logic, now we are guaranteed that there are two increasing elements which
+					// occured before num. We found increasing triplet, simply return true.
+					return true; 
+				}
+			}
+
+			return false;
+		}
 };
