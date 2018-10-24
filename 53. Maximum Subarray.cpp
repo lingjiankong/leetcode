6c9d2296e7856339fa741b875d30bf2abb89e12c
Andy Kong <andy.k@aiincorporated.com>
2018-10-23 18:20:33 -0700

    Update

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -1,10 +1,16 @@
+// This is a DP problem.
+// The maximum sum of subarray that ends at nums[i] depends on
+// 1. The maximum sum of subarray that ends at nums[i-1] (i.e. last curSum), and 
+// 2. The value of nums[i] itself.
+// Therefore, curSum = max(curSum + nums[i], nums[i]).
 int maxSubArray(vector<int>& nums)
 {
 	if (nums.size() == 0)
 	{
 		return 0;
 	}
-	
+
+	// The maximum sum of subarray that ends at nums[i]
 	int curSum = nums[0];
 	int maxSum = nums[0];
 	
