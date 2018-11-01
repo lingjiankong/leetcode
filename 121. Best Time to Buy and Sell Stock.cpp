8cb3bbe16ca46ab76516a353a35e7b37b5c15536
Andy Kong <andy.k@aiincorporated.com>
2018-11-01 09:34:40 -0700

    Stock

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -1,68 +1,63 @@
-class Solution 
+// Intuitive solution
+int maxProfit(vector<int>& prices) 
 {
+	if (prices.empty()) 
+	{
+		return 0;
+	}
+	
+	int minPrice = prices[0];
+	int maxProfit = 0;
+	
+	for (int i = 1; i < prices.size(); ++i) 
+	{
+		int profit = prices[i] - minPrice;
+		maxProfit = max(profit, maxProfit);
+		minPrice = min(prices[i], minPrice);
+	}
+	
+	return maxProfit;
+}
 
-	public:
+// DP solution. This builds the intuition for question 123. Best Time to Buy and Sell Stock III
+// 
+// At any day, we keep two state in dp:
+//
+// 1. sold: the max amount of money you have when you've sold the stock 
+// (the max amount of money you have when you have no stock in hand).
+// It doesn't matter when you sold the stock. You could have sold it before or you can sell it now. 
+// The point here is that you have NO stock in hand.
+// 
+// 2. hold: the max amount of money you have when you are holding the stock.
+// (the max amount of money you have when you have the stock in hand). 
+// In order to hold the stock, of course you should have bought the stock at some point.
+// Note here we have to spend money to buy the stock, therefore the amount of money is negative 
+// since we own other people money (we have to borrow money to buy stock).
+// If doesn't matter when you bought the stock. You could have bought it before or you can buy it now. 
+// The point here is that you currently have stock in hand.
+int maxProfit2(vector<int>& prices) 
+{
+	int sold = 0;
+	int hold = INT_MIN;
 
-		// Intuitive solution
-		int maxProfit(vector<int>& prices) 
-		{
-			if (prices.empty()) 
-			{
-				return 0;
-			}
-			
-			int minPrice = prices[0];
-			int maxProfit = 0;
-			
-			for (int i = 1; i < prices.size(); ++i) 
-			{
-				int profit = prices[i] - minPrice;
-				maxProfit = max(profit, maxProfit);
-				minPrice = min(prices[i], minPrice);
-			}
-			
-			return maxProfit;
-		}
+	for (auto price : prices) 
+	{
+		// sold is the max of either:
+		// sold <- you've sold the stock before and you currently have no stock in hand, and you do nothing
+		// hold + price <- you are holding the stock in hand, and you sell the stock now
+		sold = max(sold, hold + price);
 
-		// DP solution. This builds the intuition for question 123. Best Time to Buy and Sell Stock III
-		// 
-		// At any day, we keep two state in dp:
-		//
-		// 1. sold: the max amount of money you have when you've sold the stock 
-		// (the max amount of money you have when you have no stock in hand).
-		// If doesn't matter when you sold the stock. You could have sold it before or you can sell it now. 
-		// The point here is that you have NO stock in hand.
-		// 
-		// 2. hold: the max amount of money you have when you are holding the stock.
-		// (the max amount of money you have when you have the stock in hand). 
-		// In order to hold the stock, of course you should have bought the stock at some point.
-		// Note here we have to spend money to buy the stock, therefore the amount of money is negative 
-		// since we own other people money (we have to borrow money to buy stock).
-		// If doesn't matter when you bought the stock. You could have bought it before or you can buy it now. 
-		// The point here is that you currently have stock in hand.
-		int maxProfit2(vector<int>& prices) 
-		{
-			int hold = INT_MIN;
-			int sold = 0;
-			for (const auto& price : prices) 
-			{
-				// sold is the max of either:
-				// sold <- you've sold the stock before and you currently have no stock in hand, and you do nothing
-				// hold + price <- you are holding the stock in hand, and you sell the stock now
-				sold = max(sold, hold + price);
+		// hold is the max of either:
+		// hold <- you've bought the stock before and is currently holding it now, and you do nothing
+		// -price <- you buy the stock now
+		hold = max(hold, -price);
+	}
 
-				// hold is the max of either:
-				// hold <- you've bought the stock before and is currently holding it now, and you do nothing
-				// -price <- you buy the stock now
-				hold = max(hold, -price);
-			}
-			return sold;
-		}
-		
-		// Example
-		//
-		//		 {3,  5,  7,  0,  3,  2,  9,  1}
-		// hold	 -3  -3  -3   0   0   0   0   0
-		// sold   0   2   4   4   4   4   9   9
+	return sold;
+}
 
-};
+// Example
+//
+//		          {3,  5,  7,  1,  3,  2,  9,  1}
+// sold        0   0   2   4   4   4   4   8   8 
+// hold	 INT_MIN  -3  -3  -3  -1  -1  -1  -1  -1 
--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -1,69 +1,64 @@
-class Solution 
+// Intuitive solution
+int maxProfit(vector<int>& prices)
 {
+	if (prices.size() == 0)
+	{
+		return 0;
+	}
+
+	int maxProfit = 0;
 	
-	public:
+	for (int i = 1; i < prices.size(); ++i) 
+	{
+		int profit = prices[i] - prices[i-1];
 		
-		int maxProfit(vector<int>& prices)
+		if (profit > 0)
 		{
-			if (prices.size() == 0)
-			{
-				return 0;
-			}
-
-			int maxProfit = 0;
-			
-			for (int i = 1; i < prices.size(); ++i) 
-			{
-				int profit = prices[i] - prices[i-1];
-				
-				if (profit > 0)
-				{
-					maxProfit += profit;
-				}
-
-			}
-
-			return maxProfit;
+			maxProfit += profit;
 		}
 
-		// DP solution, compare this one with 309. Best Time to Buy and Sell Stock with Cooldown
-		// This question is just 309 without cooldown.
-		//
-		// Also, compare this question with 121. Best Time to Buy and Sell Stock
-		// This question allow unlimited transactions, 121 allows only one transaction.
-		//
-		// In this question:
-		// 
-		// sold is the maximum amount of money we have when we don't have any tock in hand.
-		// It doesn't matter if we've just sold our stock for the first time
-		// or if we've performed buy-sell actions for 100 times already.
-		// 
-		// hold is the maximum amount of money when we have a stock in hand.
-		// Again, it doesn't matter if we've bought 100 times (and sold 99 times) before
-		// or if we are holding the stock that we've bought for the very first time.
-		//
-		// However, in 121:
-		// 
-		// sold is the maximum amount of money when we've sold the stock that we've bought for THE FIRST TIME.
-		// hold is the maximum amount of money when we've are holding the stock that we bought for THE FIRST TIME.
-		// Therefore the dp transition limits us to perform only one buy-sell transaction.
-		//
-		// It is hard to explain in word, take a deeper look into the dp transition equations
-		// of the stock series problem, and look at the state transition graph on GitHub.
-		int maxProfit2(vector<int>& prices)
-		{
-			int hold = INT_MIN;
-			int sold = 0;
+	}
 
-			for (const auto& price : prices)
-			{
-				int prevSold = sold;
-				int prevHold = hold;
-				
-				sold = max(prevSold, prevHold + price);
-				hold = max(prevHold, prevSold - price);
-			}
+	return maxProfit;
+}
 
-			return sold;
-		}
-};
+// DP solution, compare this one with 309. Best Time to Buy and Sell Stock with Cooldown
+// This question is just 309 without cooldown.
+//
+// Also, compare this question with 121. Best Time to Buy and Sell Stock
+// This question allows unlimited transactions, 121 allows only one transaction.
+//
+// In this question:
+// 
+// sold is the maximum amount of money we have when we don't have any stock in hand.
+// It doesn't matter if we've just sold our stock for the first time
+// or if we've performed buy-sell actions for 100 times already.
+// 
+// hold is the maximum amount of money when we have a stock in hand.
+// Again, it doesn't matter if we've bought 100 times (and sold 99 times) before
+// or if we are holding the stock that we've bought for the very first time.
+//
+// However, in 121:
+// 
+// sold is the maximum amount of money when we've sold the stock that we've bought for THE FIRST TIME.
+// hold is the maximum amount of money when we've are holding the stock that we bought for THE FIRST TIME.
+// Therefore the dp transition limits us to perform only one buy-sell transaction.
+//
+// It is hard to explain in word, take a deeper look into the dp transition equations
+// of the stock series problem, and look at the state transition graph you drew.
+int maxProfit2(vector<int>& prices)
+{
+	int hold = INT_MIN;
+	int sold = 0;
+
+	for (auto price : prices)
+	{
+		int prevSold = sold;
+		int prevHold = hold;
+		
+		sold = max(prevSold, prevHold + price);
+		hold = max(prevHold, prevSold - price);
+	}
+
+	return sold;
+}
