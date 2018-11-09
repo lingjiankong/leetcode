872a565e39bb7cbb1d7ffbfb7313c81a2936b125
Andy Kong <andy.k@aiincorporated.com>
2018-11-08 16:54:56 -0800

    Refine

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -1,36 +1,30 @@
-class Solution
+int longestConsecutive(vector<int>& nums)
 {
 
-	public:
+	// Use unordered_set to eliminate duplicate elements
+	unordered_set<int> seen(nums.begin(), nums.end());
+	int res = 0;
 
-		int longestConsecutive(vector<int>& nums)
+	for (int num : nums)
+	{
+		// Find those that are greater than num
+		int up = num + 1;
+		while (seen.find(up) != seen.end())
 		{
-
-			// Use unordered_set to eliminate duplicate elements
-			unordered_set<int> seen(nums.begin(), nums.end());
-			int res = 0;
-
-			for (int num : nums)
-			{
-				// Find those that are greater than num
-				int up = num + 1;
-				while (seen.find(up) != seen.end())
-				{
-					seen.erase(up);
-					++up;
-				}
-
-				// Find those that are less than num
-				int down = num - 1;
-				while (seen.find(down) != seen.end())
-				{
-					seen.erase(down);
-					--down;
-				}
-
-				res = max(res, up-down-1);
-			}
-
-			return res;
+			seen.erase(up);
+			++up;
 		}
-};
+
+		// Find those that are less than num
+		int down = num - 1;
+		while (seen.find(down) != seen.end())
+		{
+			seen.erase(down);
+			--down;
+		}
+
+		res = max(res, up-down-1);
+	}
+
+	return res;
+}
--- 164. Maximum Gap.cpp
+++ 164. Maximum Gap.cpp
@@ -1,19 +1,18 @@
 // The whole idea is bucket sort
 // You put each element into the corresponding bucket.
 //
-// gap is the distance between each bucket
-// i.e. the size of each bucket
-// i.e. How big each bucket is
-// i.e. the distance within each bucket. 
+// bucketSize is the size of each bucket
+// i.e. How big each bucket is (the distance within each bucket).
 //
-// We would like to choose gap size, such that it is smaller than the
+// We would like to choose bucketSize, such that it is smaller than the
 // minimum possible max difference between any two successive elements.
 //
 // Example:
-// nums = [1, 4, 5, 8, 10, 25, 26, 27, 28, 29, 30]
-// gap = ceil(30-1)/11-1) = 3, number of bucket = nums.size()-1 = 10
+// nums = [1, 4, 5, 8, 10, 25, 26, 27, 28, 29, 33], 11 elements.
+// Mininum possible max difference, in float, is (33-1)/(11-1) = 32/10 = 3.2
+// therefore bucketSize = (33-1)/(11-1) = 3, number of bucket = (33-1)/3 + 1 = 11
 // We have the following buckets:
-// [1, 4) | [4, 7) | [7, 10) | [10, 13) | [13, 16) | [16, 19) | [19, 22) | [22, 25) | [25, 28) | [28, 30)
+// [1, 4) | [4, 7) | [7, 10) | [10, 13) | [13, 16) | [16, 19) | [19, 22) | [22, 25) | [25, 28) | [28, 31) | [33, _)
 //
 // 0th bucket: empty
 // 1th bucket: min=4, max=5
@@ -25,122 +24,88 @@
 // 7th bucket: empty
 // 8th bucket: min=25, max=27
 // 9th bucket: min=28, max=29
-// 
-// Another example
-// [ 2,      3,  4, 5,              10, 11,   12,     13	 ]
-//  (minNum) 3 | 4, 5 |      |      10, 11 |  12  |  (maxNum)
-// gap = ceil(13-2)/(8-1) = ceil(11/7) = 2
-// number of bucket = nums.size()-1 = 8-1 = 7
-// 3, 4, 5, 10, 11, 12 are put into the corresponding positon in the bucket
+// 11th bucket: min=33, max=33
 //
-// Given nums, the minimum possible max difference between any successive elemenets
+// Given nums, the minimum possible max difference between any successive elements
 // can not be smaller than (maxNum - minNum) / (n - 1).
 // Visualize it: if you have nums = [3, 4, 5, 9],
 // the minimum *possible* max difference between any two successive elements is
 // (maxNum - minNum) / (n - 1) = (9 - 3) / (4 - 1) = 6 / 3 = 2.
 // the *actual* max difference between two successive elements, in this case, is 4 (9 - 5 = 4).
 //
-// In the extreme case, nums are evenly spaced, for example when we have [3, 5, 7, 9], 
-// the minimum possible max difference between any two successive elements is 2,
+// In the extreme case, nums are evenly spaced, for example when we have [3, 5, 7, 9],
+// the minimum possible max difference between any two successive elements (9-3)/(4-1) = 2,
 // and the actual max difference between two successive elements is also 2.
 //
-// Another example
+// Another example:
 // When we have [1, 1, 2, 1, 1], the minimum possible max difference between any two successive elements
-// is (2-1)/(5-1) = 1/4, which ceil to 1, which is also the same as the
-// actual max difference between two successive elements (2 - 1 = 2).
+// is (2-1)/(5-1) = 1/4. We can't have a bucket size of 0, so we set the bucketSize to be 1. 
+// Actual max difference between two successive elements (2 - 1 = 1).
 //
-// So if we make bucket size (gap) smaller than this minimum possible max difference between any two
+// So if we make bucketSize smaller than this minimum possible max difference between any two
 // successive elements, then any difference within the same bucket is not the
-// "actual max difference between two successive elements" we are looking for, 
+// "actual max difference between two successive elements" we are looking for,
 // so we are safe to look only for the inter-bucket differences.
 //
-// Actually, if you make the gap between the buckets smaller than the
+// Actually, if you make the bucketSize between the buckets smaller than the
 // minimum possible max difference between any two successive elements,
 // everything works just fine, so making the buckets smaller doesn't affect the correctness,
-// You might just as well use gap = (maxNum-minNum)/(2n).
+// You might just as well use bucketSize = (maxNum-minNum)/(2n).
 // and make the bucket size smaller (i.e. more buckets)
-class Solution
+int maximumGap(vector<int>& nums)
 {
+	// No such "difference between successive elements" if only one element.
+	if (nums.size() < 2)
+	{
+		return 0;
+	}
 
-	public:
+	int minNum = *min_element(nums.begin(), nums.end());
+	int maxNum = *max_element(nums.begin(), nums.end());
 
-		int maximumGap(vector<int>& nums)
+	// If minNum == maxNum, max difference between two successive elements is just 0.
+	if (minNum == maxNum)
+	{
+		return 0;
+	}
+
+	int n = nums.size();
+	int bucketSize = max(1, (maxNum - minNum)/(n - 1));
+	int numberOfBuckets = (maxNum - minNum) / bucketSize + 1;
+
+	vector<int> bucketsMin(numberOfBuckets, INT_MAX); // the min of each bucket
+	vector<int> bucketsMax(numberOfBuckets, INT_MIN); // the max of each bucket
+
+	for (auto num : nums)
+	{
+		int index = (num-minNum) / bucketSize; // bucket index
+
+		// Update max and min value in each bucket.
+		bucketsMin[index] = min(num, bucketsMin[index]);
+		bucketsMax[index] = max(num, bucketsMax[index]);
+	}
+
+	int maxDifference = INT_MIN;
+
+	int prevMax = minNum;
+
+	// Iterate over all buckets)
+	for (int i = 0; i < numberOfBuckets; ++i)
+	{
+		// maxDifference candidate is the min in current bucket, minus the max in previous bucket
+		// (or previous previous previous bucket, if the last two bucket were empty, for example)
+		// We only update maxDifference if min exists in current bucket
+		if (bucketsMin[i] != INT_MAX)
 		{
-
-			// No such "difference between successive elements" if only one element.
-			if (nums.size() < 2)
-			{
-				return 0;
-			}
-
-			int n = nums.size();
-			int minNum = INT_MAX;
-			int maxNum = INT_MIN;
-
-			minNum = *min_element(nums.begin(), nums.end());
-			maxNum = *max_element(nums.begin(), nums.end());
-			
-			// If minNum == maxNum, max difference between two successive elements is just 0.
-			// 
-			// Also, if minNum == maxNum, gap = (int)(ceil(((double)(maxNum-minNum)/(n-1)))) will be zero.
-			// Later when we select bucket index we will have index = (num-minNum)/gap 
-			// which is devided by zero. We do not want that.
-			if (minNum == maxNum)
-			{
-				return 0;
-			}
-
-			// Why do we have std::ceil? Because it avoids the case when gap = 0.
-			// We've tested if (minNum == maxNum) above so now maxNum-minNum must be greater than 0,
-			// therefore ceil anything greater than 0 will give us 1.
-			//
-			// std::ceil takes floating point values, that's why we cast our integer devision result to double
-			int gap = (int)(ceil(((double)(maxNum-minNum)/(n-1))));
-
-			vector<int> bucketsMin(n-1, INT_MAX); // the min of each bucket
-			vector<int> bucketsMax(n-1, INT_MIN); // the max of each bucket
-
-			for (auto num : nums)
-			{
-				if (num == minNum || num == maxNum)
-				{
-					// we don't want to put minNum and maxNum in the bucket.
-					// We will deal with them in the next loop
-					continue;
-				}
-
-				int index = (num-minNum) / gap; // bucket index
-				
-				// Update max and min value in each bucket.
-				bucketsMin[index] = min(num, bucketsMin[index]);
-				bucketsMax[index] = max(num, bucketsMax[index]);
-			}
-
-			int maxDifference = INT_MIN;
-
-			int prevMax = minNum;
-
-			// Iterate over all buckets (we have n-1 buckets)
-			for (int i = 0; i < n-1; ++i)
-			{
-				// maxDifference candidate is the min in current bucket, minus the max in previous bucket
-				// (or previous previous previous bucket, if the last two bucket were empty, for example)
-				// We only update maxDifference if min exists in current bucket
-				if (bucketMin[i] != INT_MAX)
-				{
-					maxDifference = max(maxDifference, bucketsMin[i] - prevMax);
-				}
-				
-				// We only update prevMax if max exists in current bucket
-				if (bucketMax[i] != INT_MIN)
-				{
-					prevMax = max(prevMax, bucketsMax[i]);
-				}
-			}
-
-			// Don't forget maxNum
-			maxDifference = max(maxDifference, maxNum - prevMax);
-
-			return maxDifference;
+			maxDifference = max(maxDifference, bucketsMin[i] - prevMax);
 		}
-};
+
+		// We only update prevMax if max exists in current bucket
+		if (bucketsMax[i] != INT_MIN)
+		{
+			prevMax = max(prevMax, bucketsMax[i]);
+		}
+	}
+
+	return maxDifference;
+}
