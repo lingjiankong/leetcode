7329e53e00d28539816e578b74bcd68c172ecc0e
Andy Kong <andy.k@aiincorporated.com>
2018-11-08 18:16:46 -0800

    minor

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -2,7 +2,7 @@
 // You can do that if you have time in the future.
 //
 // Nums is unsorted, but we show sorted nums for clarity. Our algorithm works with unsorted nums.
-// Note that we are guaranteed that there is only one duplicate number in the array, but it could be repeated more than once.
+// Note that we are guaranteed that there is only *one* duplicate number in the array, but it could be repeated more than once.
 // So something like [1, 2, 3, 3, 3, 4, 4, 4] is not possible.
 //
 // Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive)
@@ -24,46 +24,42 @@
 // If count > mid, then there are more than mid elements in the range [1, mid].
 // That is, there are more than mid element in a range of size (mid-1)+1 = mid.
 // Thus this range must contain a duplicate.
-// 
+//
 // If count <= mid, then there are n+1-count elements in the range [mid+1, n].
 // That is, at least n+1-mid elements in a range of size (n-(mid+1)+1 = n-mid.
 // Thus that range must contain a duplicate.
 //
 // In another word, We know that the whole range is "too crowded" and thus that the first half or the second half of the range
 // is too crowded. So you check to know whether the first half is too crowded, and if it isn't, you know that the second half is.
-class Solution
+int findDuplicate(vector<int>& nums)
 {
+	int low = 0;
+	int high = nums.size() - 1;
 
-	public:
+	while (low <= high)
+	{
+		int mid = low + (high-low)/2;
+		int count = 0;
 
-		int findDuplicate(vector<int>& nums)
+		for (int num : nums)
 		{
-			int low = 0;
-			int high = nums.size() - 1;
-			while (low <= high)
+			if (num <= mid)
 			{
-				int mid = low + (high-low)/2;
-				int count = 0;
-
-				for (int num : nums)
-				{
-					if (num <= mid)
-					{
-						++count;
-					}
-				}
-
-				// Left half too crowded.
-				if (count > mid)
-				{
-					high = mid - 1;
-				}
-				// Right half too crowded.
-				else
-				{
-					low = mid + 1;
-				}
+				++count;
 			}
-			return low;
 		}
-};
+
+		// Left half too crowded.
+		if (count > mid)
+		{
+			high = mid - 1;
+		}
+		// Right half too crowded.
+		else
+		{
+			low = mid + 1;
+		}
+	}
+
+	return low;
+}
