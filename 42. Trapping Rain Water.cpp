326c9f7f750dbc2f0e849ea8aafaadccf575a4cf
Andy Kong <andy.k@aiincorporated.com>
2018-11-28 18:14:43 -0800

    Revise old verbose stuff

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -12,33 +12,42 @@
 // ^                              ^
 // L                              R
 //
+// DP solution, very intuitive.
+// For current position i, keep track of its left max height and right max height.
+int trap(vector<int>& height)
+{
+	if (height.empty())		 
+	{
+		return 0;
+	}
+
+	vector<int> leftMaxHeight(height.size(), 0);
+	vector<int> rightMaxHeight(height.size(), 0);
+
+	for (int i = 1; i < height.size(); ++i)
+	{
+		leftMaxHeight[i] = max(leftMaxHeight[i-1], height[i-1]);
+	}
+
+	for (int j = height.size() - 2; j >= 0; --j)
+	{
+		rightMaxHeight[j] = max(rightMaxHeight[j+1], height[j+1]);
+	}
+
+	int maxWater = 0;
+	for (int i = 1; i < height.size() - 1; ++i)
+	{
+		int minHeight = min(leftMaxHeight[i], rightMaxHeight[i]);
+		maxWater += min(0, minHeight - height[i]);
+	}
+
+	return maxWater;
+}
+
 // Maintain two pointers left and right, we keep track of the maximum height of left wall and right wall.
 // The amount of water we can fill at certain position is controlled by:
 // 1. The height at current position,
 // 2. Minimum of (max height of walls to the left, max height of walls to the right)
-//
-// Let say now L = 0 and R = 99, the first time we enter the while loop, think about height[99] (i.e. height[right])
-// being very high, say, higher than height[left] at position from 0, 1, 2, 3, 4, 5, 
-// then rightMaxHeight will also always be greater than leftMaxHeight. 
-// Here we enter if (height[left] < height[right]) every time,
-// and the amount of water we can fill at position 0 to 5 is completely determined by leftMaxHeight - height[left]
-// (since rightMaxHeight, is always greater than leftMaxHeight).
-// The right side will never be the constraint (i.e. shorter edge never happens on the right side)).
-//
-// Let's say at position 6, height[left] (which is height[6]) becomes super large, larger than current height[right],
-// now maxHeightLeft is greater than maxHeightRight.
-// The condition height[left] >= height[right] is true and we enter the else loop.
-// Thus, the same theory applies here: now the left side is never going to be the constraint,
-// and now the amount of water we can fill is completely determined by rightMaxHeight - height[right].
-//
-// The process repeats, L pointer going right, R pointer going left,
-// until they overlap is when we've finished calculating the amount of water we can fill at all positions
-//
-// Things to keep in mind: if height[left] < height[right], then it must be the case that leftMaxHeight < rightMaxHeight,
-// Becuase leftMaxHeight and rightMaxHeight are assigned right in the beginning when we see a new height[left] and height[right].
-// meaning the amount of water we can fill at current height[left] can be determined only using
-// leftMaxHeight - height[left]. Same is true vice versa if height[left] >= height[right].
-
 int trap(vector<int>& height)
 {
 
@@ -59,7 +68,7 @@ int trap(vector<int>& height)
 		leftMaxHeight = max(leftMaxHeight, height[left]);
 		rightMaxHeight = max(rightMaxHeight, height[right]);
 
-		if (height[left] < height[right])
+		if (leftMaxHeight < rightMaxHeight)
 		{
 			total += leftMaxHeight - height[left];
 			++left;
@@ -74,36 +83,22 @@ int trap(vector<int>& height)
 	return total;
 }
 
+// Using stack
+// TODO: Unfinished
 int trap(vector<int>& height)
 {
+	stack<int> heightStack;
 
-	int total = 0;
-
-	// Well, you can think of leftMaxHeight = height[left] and rightMaxHeight = height[right]
-	// We assign 0 to them just simply because we don't need to check the size of the vector.
-	// Otherwise we would need to make sure height.size() != 0.
-	int leftMaxHeight = 0;
-	int rightMaxHeight = 0;
-
-	int left = 0;
-	int right = height.size() - 1;
-
-	while (left < right)
+	for (int i = 0; i < height.size(); ++i)
 	{
-		leftMaxHeight = max(leftMaxHeight, height[left]);
-		rightMaxHeight = max(rightMaxHeight, height[right]);
-
-		if (height[left] < height[right])
+		if (heightStack.empty() || height[i] <= height[heightStack.top()])	
 		{
-			total += leftMaxHeight - height[left];
-			++left;
+			heightStack.push(i);
 		}
 		else
 		{
-			total += rightMaxHeight - height[right];
-			--right;
+				
 		}
 	}
 
-	return total;
 }
