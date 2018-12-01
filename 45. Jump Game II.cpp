77138206f3e5373a2f765880b4ad66c989431f57
Andy Kong <andy.k@aiincorporated.com>
2018-12-01 00:56:04 -0800

    Review more

--- 45. Jump Game II.cpp
+++ 45. Jump Game II.cpp
@@ -1,3 +1,19 @@
+// ***
+//
+// Given an array of non-negative integers, you are initially positioned at the first index of the array.
+// Each element in the array represents your maximum jump length at that position.
+// Your goal is to reach the last index in the minimum number of jumps.
+// 
+// Example:
+// 
+// Input: [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// 
+// Note: You can assume that you can always reach the last index.
+//
+// ***
+//
 // Note that the problem guarantees that that we are can always jump to the last index.
 // Therefore no need to check if we can jump to current index.
 int jump(vector<int>& nums)
