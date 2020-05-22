6259fb14a0e0f7d7c7276cd4ed05e1efd73461e6
Lingjian Kong <lingjian.kong@bluerivert.com>
2020-05-21 23:42:49 -0700

    chi tou liang dao ti

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -1,14 +1,16 @@
 // ***
 //
-// Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
-// You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
-// 
+// Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very
+// right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return
+// the max element in the sliding window.
+//
 // Example:
-// 
+//
 // Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
-// Output: [3,3,5,5,6,7] 
-// Explanation: 
-// 
+// Output: [3,3,5,5,6,7]
+//
+// Explanation:
+//
 // Window position                Max
 // ---------------               -----
 // [1  3  -1] -3  5  3  6  7       3
@@ -18,118 +20,99 @@
 //  1  3  -1  -3 [5  3  6] 7       6
 //  1  3  -1  -3  5 [3  6  7]      7
 //
-// Note: 
+// Note:
 // You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
 //
 // ***
-//
+
 // 1. Brute force
 // Time complexity: O((n – k) * k)
-vector<int> maxSlidingWindow(vector<int>& nums, int k)
-{
-	// Need to check for empty nums due to *max_element
-	if (nums.empty())
-	{
-		return {};
-	}
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> toReturn;
 
-	vector<int> toReturn;
-	
-	for (int i = 0; i <= nums.size() - k; ++i)
-	{
-		int maxElement = *max_element(nums.begin() + i, nums.begin() + i + k);
-		toReturn.push_back(maxElement);
-	}
+    for (int i = 0; i <= nums.size() - k; ++i) {
+        int maxElement = *max_element(nums.begin() + i, nums.begin() + i + k);
+        toReturn.push_back(maxElement);
+    }
 
-	return toReturn;
+    return toReturn;
 }
 
 // 2. BST (multiset), the most intuitive solution.
 // Time complexity: O((n – k + 1) * logk)
-vector<int> maxSlidingWindow(vector<int>& nums, int k)
-{
-	vector<int> toReturn;
-	multiset<int> window;
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> toReturn;
+    multiset<int> window;
 
-	for (int i = 0; i < nums.size(); ++i)
-	{
-		window.insert(nums[i]);
+    for (int i = 0; i < nums.size(); ++i) {
+        window.insert(nums[i]);
 
-		// Initially, the window builds up, we just add elements to the multiset.
-		// However, when we've reached a "full window", we start removing elements to the left
-		// of the left bound of the sliding window.
-		if (i - k + 1 >= 0)
-		{
-			// Iterator denoted by rbegin() is the biggest element in the multiset.
-			toReturn.push_back(*window.rbegin());
-			
-			// Remove the element no longer in the window. 
-			auto itr = window.find(nums[i - k + 1]);
-			window.erase(itr);
-		}
-	}
-		
-	return toReturn; 
+        // Initially, the window builds up, we just add elements to the multiset. However, when we've reached a "full
+        // window", we start removing elements to the left of the left bound of the sliding window. Here, i - k is the
+        // element to the left of of the left bound of the current window, i - k + 1 is therefore the first element in
+        // current window. We first calculate the max value in current window by getting the biggest element denoted by
+        // rbegin() and push the result to toReturn. After this is done, the first element in the window is no longer
+        // needed, and we thus erase that element from the multiset.
+        if (i - k + 1 >= 0) {
+            // Iterator denoted by rbegin() is the biggest element in the multiset.
+            toReturn.push_back(*window.rbegin());
+
+            // Remove the element no longer needed in the window.
+            auto itr = window.find(nums[i - k + 1]);
+            window.erase(itr);
+        }
+    }
+
+    return toReturn;
 }
 
-// 3. Monotonic priority queue (using deque as underlying data structure)
+// 3. Monotonic queue in non-ascending order (using deque as underlying data structure)
+// A monotonic Queue is a data structure the elements from the front to the end is strictly either increasing or
+// decreasing.
 // Time complexity: O(n)
-class MonotonicQueue
-{
+class MonotonicQueue {
+public:
+    // Push an element e to the monotonic  queue.
+    // All elements smaller than e will be popped from the queue,
+    // so the queue will always be sorted in non-ascending order,
+    // with the largest element on the left and smallest element on the right.
+    // The underlying deque looks something like this: (9, 7, 7, 4, 1}
+    void push(int e) {
+        while (!_data.empty() && e > _data.back()) {
+            _data.pop_back();
+        }
 
-	public:
+        _data.push_back(e);
+    }
 
-		// Push an element e to the priority queue.
-		// All elements smaller than e will be popped from the queue,
-		// so the queue will always be sorted in non-ascending order.
-		// The underlying deque looks something like this: (9, 7, 7, 4, 1}
-		void push(int e)
-		{
-			while (!mData.empty() && e > mData.back())
-			{
-				mData.pop_back();
-			}
+    // Pop the max element
+    void popMax() { _data.pop_front(); }
 
-			mData.push_back(e);
-		}
+    // Peek the max element
+    int peekMax() const { return _data.front(); }
 
-		// Pop the max element
-		void popMax()
-		{
-			mData.pop_front();
-		}
-
-		// Peek the max element
-		int peekMax() const
-		{
-			return mData.front();
-		}
-	
-	private:
-		
-		deque<int> mData;
-
+private:
+    deque<int> _data;
 };
 
-vector<int> maxSlidingWindow(vector<int>& nums, int k)
-{
-	vector<int> toReturn;
-	MonotonicQueue q;
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> toReturn;
+    MonotonicQueue q;
 
-	for (int i = 0; i < nums.size(); ++i)
-	{
-		q.push(nums[i]);
+    for (int i = 0; i < nums.size(); ++i) {
+        q.push(nums[i]);
 
-		if (i - k + 1 >= 0)
-		{
-			toReturn.push_back(q.peekMax());
+        if (i - k + 1 >= 0) {
+            toReturn.push_back(q.peekMax());
 
-			if (nums[i-k+1] == q.peekMax())
-			{
-				q.popMax();
-			}
-		}
-	}
+            // Only pop the max element if it equals to the element that we no longer need in the window.
+            // There might be multiple max element with the samle value. We only pop one of them.
+            if (nums[i - k + 1] == q.peekMax()) {
+                q.popMax();
+            }
+        }
+    }
 
-	return toReturn;
+    return toReturn;
 }
+
--- 253. Meeting Rooms II.cpp
+++ 253. Meeting Rooms II.cpp
@@ -1,88 +1,90 @@
 // ***
 //
-// Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
-// find the minimum number of conference rooms required.
+// Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the
+// minimum number of conference rooms required.
 //
 // Example 1:
-//
 // Input: [[0, 30],[5, 10],[15, 20]]
 // Output: 2
 //
 // Example 2:
-//
-// Input: [[7,10],[2,4]]
+// Input: [[7, 10],[2, 4]]
 // Output: 1
 //
-// Definition for an interval.
-// struct Interval {
-//       int start;
-//       int end;
-//       Interval() : start(0), end(0) {}
-//       Interval(int s, int e) : start(s), end(e) {}
-// };
-//
 // ***
-//
-// Appraoch: Sort both start and end.
+
+// Sort both start and end.
 //
 // |__________|
 //				|_______|
 //					|__________|
 //					  |__________|
 //							|______|
-
-int minMeetingRooms(vector<Interval>& intervals) {
-    vector<int> starts;
-    vector<int> ends;
+//
+int minMeetingRooms(vector<vector<int>>& intervals) {
+    vector<int> startTimes;
+    vector<int> endTimes;
 
     for (auto interval : intervals) {
-        starts.push_back(interval.start);
-        ends.push_back(interval.end);
+        startTimes.push_back(interval[0]);
+        endTimes.push_back(interval[1]);
     }
 
-    sort(starts.begin(), starts.end());
-    sort(ends.begin(), ends.end());
+    sort(startTimes.begin(), startTimes.end());
+    sort(endTimes.begin(), endTimes.end());
 
     // end: index of current end
-    // ends[end]: the earliest time when one of the meetings will end
-    // There might be several meeting going on at the same time,
-    // and we want the know when the earliest time one of the meetings will end,
-    // so if a meeting has ended, we can put the meeting that starts now in that room which has been vacanted.
-    int end = 0;
+    // endTimes[end]: the earliest time when one of the meetings will end
+    // There might be several meeting going on at the same time, and we want to know when the earliest time one of the
+    // meetings will end, so if a meeting has ended, we can put the meeting that starts now in that room which has just
+    // been vacanted.
+    int endIdx = 0;
 
     // total: Total meeting rooms
     int total = 0;
 
     for (int i = 0; i < intervals.size(); ++i) {
         // No previous meeting room has been vacanted. Must open a new room.
-        if (starts[i] < ends[end]) {
+        if (startTimes[i] < endTimes[endIdx]) {
             ++total;
         }
-        // If starts[i] >= ends[end], a meeting room has just been vacanted.
-        // We can use that meeting room. ++end.
+        // If startTimes[i] >= endTimes[endIdx], a meeting room has just been vacanted.
+        // We can use that meeting room. ++endIdx.
         else {
-            ++end;
+            ++endIdx;
         }
     }
 
     return total;
 }
 
-// Using priority_queue which stores the *end time* (the earliest end time has highest priority) of each meeting.
-int minMeetingRooms(vector<Interval>& intervals) {
-    priority_queue<int, vector<int>, greater<int>> pq;
+// Use priority queue
+int minMeetingRooms(vector<vector<int>>& intervals) {
+    // Sort intervals by start time
+    sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) { return a[0] < b[0]; });
 
-    sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) { return a.start < b.start; });
+    // Using priority_queue to store the *end time* (the earliest end time has highest priority) of each meeting.
+    priority_queue<int, vector<int>, greater<int>> pqEndTimes;
+
     for (auto interval : intervals) {
-        // If pq.top() <= interval.start, then there's a meeting that has just ended, in which case we can use that
-        // vacanted room. No need for extra room.
-        if (!pq.empty() && pq.top() <= interval.start) {
-            pq.pop();
+        // If pqEndTimes.top() (earliest end time of exisiting meetings) <= interval[0] (start time of current meeting),
+        // then there's a meeting that has just ended, in which case we can use that vacanted room. No need for extra
+        // room.
+        if (!pqEndTimes.empty() && pqEndTimes.top() <= interval[0] /* start time of current meeting */) {
+            pqEndTimes.pop();
         }
 
-        pq.push(interval.end);
+        // Regardless of using previously vacanted room or open a new room, we need to push the end time of current
+        // meeting to the priority queue
+        pqEndTimes.push(interval[1] /* end time of current meeting */);
     }
 
-    return pq.size();
+    // Explanation why pdEndTimes.size() is the minimum number of meeting rooms:
+    // Every time your new meeting can use a previously vacanted room, you pop the earliest end time out, and you push
+    // the end time for your new meeting in (pq size -1 (pop), then +1 (push)). However when you do not have a vancated
+    // room to use, you only push the end time for your new meeting in without poping any end time out (size +1 (push)).
+    // So in the end the size of pq tracts the total number of meeting rooms you have opened when you've finished
+    // traversing all the meetings.
+    return pqEndTimes.size();
 }
 
