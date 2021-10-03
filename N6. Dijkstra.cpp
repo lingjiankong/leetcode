25993d3b53e435f2950ce8592f99748eb7c4e6f4
Lingjian Kong <lingjian.kong@bluerivertech.com>
2021-10-03 09:59:57 -0700

    .

--- N6. Dijkstra.cpp
+++ N6. Dijkstra.cpp
@@ -9,15 +9,14 @@ unordered_map<T, int> dijkstra(unordered_map<T, unordered_set<T>>& neighbors,
                                unordered_map<T, unordered_map<T, int>>& weights, T start) {
     struct NodeState {
         T id;
-        int distFromStart;
+        int dist;  // distance from start the current node
     };
 
-    // min distance from start to each node
-    unordered_map<T, int> minDistTo;
+    unordered_map<T, int> minDistTo;  // min distance from start to each node
     minDistTo[start] = 0;
 
     // smallest distance has the highest priority
-    auto compare = [](const NodeState& a, const NodeState& b) { return a.distFromStart > b.distFromStart; };
+    auto compare = [](const NodeState& a, const NodeState& b) { return a.dist > b.dist; };
     priority_queue<NodeState, vector<NodeState>, decltype(compare)> pq(compare);
     pq.push({start, 0});
 
@@ -26,7 +25,7 @@ unordered_map<T, int> dijkstra(unordered_map<T, unordered_set<T>>& neighbors,
         pq.pop();
 
         T curID = curState.id;
-        int distToCur = curState.distFromStart;
+        int distToCur = curState.dist;
 
         // already obtained a shorter distance to reach curID, continue.
         if (minDistTo.count(curID) and distToCur > minDistTo[curID]) {
@@ -34,7 +33,7 @@ unordered_map<T, int> dijkstra(unordered_map<T, unordered_set<T>>& neighbors,
         }
 
         for (int neighID : neighbors[curID]) {
-            int distToNeigh = minDistTo[curID] + weights[curID][neighID];
+            int distToNeigh = distToCur + weights[curID][neighID];
 
             if (not minDistTo.count(neighID) or distToNeigh < minDistTo[neighID]) {
                 minDistTo[neighID] = distToNeigh;
